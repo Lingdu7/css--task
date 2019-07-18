e952c4b384cae3546bede2a38cf120b6704a0146
zero <yfc666@qq.vip.com>
2019-07-18 16:26:18 +0800

    123

--- play/shiyan.js
+++ play/shiyan.js
@@ -1,8 +1,8 @@
 function aaa(){
 var aa=["喊喊","肉肉","傻子","刺激"];
 var bb=["厉害","无敌","宅","追剧"]
-var a=Math.floor(Math.random()*(3))
-var b=Math.floor(Math.random()*(3))
+var a=Math.floor(Math.random()*(4))
+var b=Math.floor(Math.random()*(4))
 document.getElementById("pp").innerHTML="黄<strong style='color:orange'>"+aa[a]+"</strong>"+"你真<strong style='color:red'>"+bb[b]+"</strong>";
 var set=setTimeout("aaa()",270);
 }
